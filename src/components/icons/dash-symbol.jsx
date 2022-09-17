be550360cc07206439dbc9098950c369524d2778
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-09-17 18:08:31 -0300

    Add icons as components

---
+++ src/components/icons/dash-symbol.jsx
@@ -0,0 +1,12 @@
+export function DashSymbol () {
+  return (
+    <span style='display: inline-block; height: inherit; width: inherit;'>
+      <svg xmlns='http://www.w3.org/2000/svg' x='0' y='0' viewBox='0 0 52 52'>
+        <path
+          fill='currentColor'
+          d='M50 27.5c0 .8-.7 1.5-1.5 1.5h-45c-.8 0-1.5-.7-1.5-1.5v-3c0-.8.7-1.5 1.5-1.5h45c.8 0 1.5.7 1.5 1.5v3z'
+        ></path>
+      </svg>
+    </span>
+  );
+}
---
+++ src/components/icons/equal-symbol.jsx
@@ -0,0 +1,19 @@
+export function EqualSymbol () {
+  return (
+    <span style='display: inline-block; height: inherit; width: inherit;'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        x='0'
+        y='0'
+        enableBackground='new 0 0 42 42'
+        version='1.1'
+        viewBox='0 0 42 42'
+        xmlSpace='preserve'
+      >
+        <g fill='currentColor'>
+          <path d='M4.941 18H37.059C39.776 18 42 15.718 42 13s-2.224-5-4.941-5H4.941C2.224 8 0 10.282 0 13s2.224 5 4.941 5zM37.059 24H4.941C2.224 24 0 26.282 0 29s2.224 5 4.941 5H37.059C39.776 34 42 31.718 42 29s-2.224-5-4.941-5z'></path>
+        </g>
+      </svg>
+    </span>
+  );
+}
