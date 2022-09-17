0bf9f90e20b9e2c82c82b8d92892b8c6d79a0eb0
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-09-17 18:10:03 -0300

    Implement Calculator component on app

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,14 @@
+import {Calculator} from './components/calculator'
+
 export function App () {
   return (
-    <>
-    </>
+    <main class='grid grid-cols-2'>
+    <section class='border h-screen flex justify-center items-center'>
+        <article class="aspect-[9/16] w-full max-w-[320px] rounded-3xl border border-neutral-900/30 dark:border-gray-50/30 bg-gray-100 dark:bg-neutral-900 flex flex-col overflow-hidden">
+          <Calculator />
+        </article>
+    </section>
+    <section class='border h-screen' />
+  </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { render } from 'preact';
 import { App } from './app';
+import './helpers/setup-prefer-color-scheme';
 import './index.css';
 import './assets/index.css';
 
