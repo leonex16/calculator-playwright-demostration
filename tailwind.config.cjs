7c51af4bda1bdbb065348cc2f5392bce6b6d6154
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-09-17 16:17:31 -0300

    init project

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
