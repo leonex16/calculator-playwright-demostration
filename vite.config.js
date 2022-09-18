32e47deae961023fa8cd1f7c9a227068c19448bf
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-09-17 22:28:34 -0300

    Set base property on vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  base: 'https://leonex16.github.io/calculator-playwright-demostration/',
+  plugins: [preact()]
 })
