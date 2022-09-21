73233cf925360ba5a5baa788b63bd68082ad8283
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-09-21 09:11:21 -0300

    Failing a test

--- tests/e2e/example.test.js
+++ tests/e2e/example.test.js
@@ -27,7 +27,7 @@ test('My firstt e2e test', async ({ browser }) => {
 
     const visorValue = await calculatorVisor.evaluate(($input) => $input.value);
 
-    expect(visorValue).toBe('41');
+    expect(visorValue).toBe('42');
   } finally {
     await context.tracing.stop({ path: 'trace.zip' });
   }
